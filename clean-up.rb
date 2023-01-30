0fea051c6dce5413141b9c94fec93aaec15f759e
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-30 11:04:03 +0900

    creating folder done

--- clean-up.rb
+++ clean-up.rb
@@ -4,16 +4,32 @@ require 'fileutils'
 downloads_folder = "/Users/mattiaswelamsson/Downloads"
 
 Dir.foreach(downloads_folder) do |entry|
+  ## PATH.
   path = "#{downloads_folder}/#{entry}"
   last_edited = File.mtime(path)
   year = last_edited.strftime("%Y")
-  # next if entry == "." || entry == ".."
-  if File.directory?(path)
-    puts "#{entry} is a directory and was last edited #{year}"
-  elsif File.file?(path)
-    puts "#{entry} is a FILE and was last edited #{year}"
-    puts path
+  next if entry == "." || entry == ".."
+
+  new_directory = ("#{downloads_folder}/#{year}")
+
+  ## CREATES YEARLY FOLDER IF IT DOESN'T EXIST
+  unless File.directory?(new_directory)
+    puts "Create Folder"
+    Dir.mkdir(new_directory)
   end
+
+
+  if File.directory?(path)
+    # puts "\#{entry} is a DIRECTORY and was last edited #{year}\n"
+
+  elsif File.file?(path)
+    if year === "2022"
+      FileUtils.mv(path,new_directory)
+    end
+    puts "#{entry} is a FILE and was last edited #{year}"
+  end
+
+
 end
 
 
