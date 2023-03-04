59850b6ceb5f17c1dfb2fc4ad4b5327b55fb9ff9
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-04 17:14:23 +0900

    options added

--- clean_up.rb
+++ clean_up.rb
@@ -1,7 +1,32 @@
 require 'find'
 require 'fileutils'
 
+puts "Please choose the folder which you wish to clean up:"
+# puts Dir.glob(Dir.home + "/*")
+# List the three as options
+# Give an option to pick one [0, 3, 9]
+
 downloads_folder = File.join(Dir.home, 'Downloads')
+documents_folder = File.join(Dir.home, 'Documents')
+desktop_folder = File.join(Dir.home, 'Desktop')
+puts "[0] - " + File.basename(downloads_folder)
+puts "[3] - " + File.basename(documents_folder)
+puts "[9] - " + File.basename(desktop_folder)
+
+user_input = gets
+user_choice = ""
+if user_input == '0'
+  user_choice = downloads_folder
+elsif user_input == '3'
+  user_choice = documents_folder
+elsif user_input == '9'
+  user_choice = desktop_folder
+else  puts "Please choose the folder which you wish to clean up:"
+puts "[0] - " + File.basename(downloads_folder)
+puts "[3] - " + File.basename(documents_folder)
+puts "[9] - " + File.basename(desktop_folder)
+end
+
 ## Arrays used in the filtering logic.
 yearly_folders = ["2019", "2020", "2021", "2022", "2023", "2024", "2025"]
 
@@ -31,16 +56,16 @@ def move_file(file, directory)
   FileUtils.mv(file, final_directory) unless file === directory
 end
 
-def organize_downloads(downloads_folder, yearly_folders)
-  Dir.foreach(downloads_folder) do |file|
+def organize_downloads(user_choice, yearly_folders)
+  Dir.foreach(user_choice) do |file|
     next if file == '.' || file == '..' || yearly_folders.include?(file)
 
-    path = File.join(downloads_folder, file)
+    path = File.join(user_choice, file)
     next unless File.exist?(path)
 
     last_edited = File.mtime(path)
     year = last_edited.strftime('%Y')
-    new_directory = create_directory(downloads_folder, year)
+    new_directory = create_directory(user_choice, year)
 
     move_file(path, new_directory)
   end
